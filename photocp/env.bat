76362d2cd392ba975021106e1acfdfc7f37ad261
jeleb <jean.edmond.bulle@gmail.com>
2014-07-26 23:44:14 +0200

    to not loose env.bat

---
+++ photocp/env.bat
@@ -0,0 +1,1 @@
+set PATH=%PATH%;C:\Program Files\Microsoft Platform SDK for Windows Server 2003 R2\Bin\;C:\devGit\photocp>set PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE
